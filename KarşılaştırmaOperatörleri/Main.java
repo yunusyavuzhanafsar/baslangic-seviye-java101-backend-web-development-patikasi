a0d77db9842943bd66a2332837f9856ebc1afb7a
yunusyavuzhanafsar <160525505+yunusyavuzhanafsar@users.noreply.github.com>
2024-03-06 16:48:58 +0300

    KarşılaştırmaOperatorleri

---
+++ KarşılaştırmaOperatörleri/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Degişkenlerin tanımlanması.
+
+        int A=10,B=20,C=10,D=40;
+
+        //Eşittir operatörü.
+
+        System.out.println(A==B);
+        System.out.println(A==C);
+        System.out.println(A==D);
+        System.out.println(B==C);
+        System.out.println(B==D);
+        System.out.println(C==D);
+        System.out.println("=================================");
+
+        //Eşit değildir operatörü.
+
+        System.out.println(A!=B);
+        System.out.println(A!=C);
+        System.out.println(A!=D);
+        System.out.println(B!=C);
+        System.out.println(B!=D);
+        System.out.println(C!=D);
+        System.out.println("=================================");
+
+        //Büyüktür operatörü.
+
+        System.out.println(A>B);
+        System.out.println(A>C);
+        System.out.println(A>D);
+        System.out.println(B>C);
+        System.out.println(B>D);
+        System.out.println(C>D);
+        System.out.println("=================================");
+
+        //Küçüktür operatörü.
+
+        System.out.println(A<B);
+        System.out.println(A<C);
+        System.out.println(A<D);
+        System.out.println(B<C);
+        System.out.println(B<D);
+        System.out.println(C<D);
+        System.out.println("=================================");
+
+        //Büyüktür eşittir operatörü.
+
+        System.out.println(A>=B);
+        System.out.println(A>=C);
+        System.out.println(A>=D);
+        System.out.println(B>=C);
+        System.out.println(B>=D);
+        System.out.println(C>=D);
+        System.out.println("=================================");
+
+        //Küçüktür eşittir operatörü.
+
+        System.out.println(A<=B);
+        System.out.println(A<=C);
+        System.out.println(A<=D);
+        System.out.println(B<=C);
+        System.out.println(B<=D);
+        System.out.println(C<=D);
+        System.out.println("=================================");
+    }
+}
